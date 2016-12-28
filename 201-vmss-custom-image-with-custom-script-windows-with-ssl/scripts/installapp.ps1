0b24396d6da8047ac86cd17cec2347d3bf1b06dd
Guy Bertental <guybartal@gmail.com>
2016-12-28 13:20:55 +0200

    debug custom script

--- 201-vmss-custom-image-with-custom-script-windows-with-ssl/scripts/installapp.ps1
+++ 201-vmss-custom-image-with-custom-script-windows-with-ssl/scripts/installapp.ps1
@@ -1,2 +1,7 @@
+"current subscription" |  Out-File 'c:\file.txt' -Append
+Get-AzureRmSubscription |  Out-File 'c:\file.txt' -Append
+"current username"  |  Out-File 'c:\file.txt' -Append
+[Environment]::UserName |  Out-File 'c:\file.txt' -Append
 $value = Get-AzureKeyVaultSecret -Name key1 -VaultName gubertkeyvault
-$value.SecretValueText | Set-Content 'c:\file.txt'
+"keyvault(key1)=" | Out-File 'c:\file.txt' -Append
+$value.SecretValueText | Out-File 'c:\file.txt' -Append
