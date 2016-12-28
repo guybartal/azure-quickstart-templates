4e07019e745c2b10d9916f16d024e354cbf40c7f
Guy Bertental <guybartal@gmail.com>
2016-12-28 15:20:47 +0200

    get certificate from key vault

---
+++ 201-vmss-custom-script-windows/scripts/keyvault.ps1
@@ -0,0 +1,7 @@
+ param (
+    [Parameter(Mandatory=$true)][string]$vaultname,
+    [Parameter(Mandatory=$true)][string]$rgname,
+    [Parameter(Mandatory=$true)][string]$secretname,
+    [Parameter(Mandatory=$true)][string]$certfile,
+    
+ )
