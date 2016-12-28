0a42a53e9962572a48308072572c71c1a1737866
Guy Bertental <guybartal@gmail.com>
2016-12-28 16:37:40 +0200

    add secret password

--- 201-vmss-custom-image-with-custom-script-windows-with-ssl/scripts/installapp.ps1
+++ 201-vmss-custom-image-with-custom-script-windows-with-ssl/scripts/installapp.ps1
@@ -1,3 +1,8 @@
+ param (
+    [Parameter(Mandatory=$true)][string]$websitepassword
+ )
+'websitepassword=' | Out-File 'c:\file.txt' -Append
+$websitepassword |  Out-File 'c:\file.txt' -Append
 "current subscription" |  Out-File 'c:\file.txt' -Append
 Get-AzureRmSubscription |  Out-File 'c:\file.txt' -Append
 "current username"  |  Out-File 'c:\file.txt' -Append
